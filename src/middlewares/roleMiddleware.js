fa13787347433dcdb11776cfbe2259fa5e15a4f3
Dushyant Goyal <goyaldushyant075@gmail.com>
2026-03-30 16:53:12 +0530

    Resolved role based authorization

--- src/middlewares/roleMiddleware.js
+++ src/middlewares/roleMiddleware.js
@@ -1,13 +1,11 @@
-exports.authorizeRoles = (...roles)=>{
+exports.authorize = (...roles) => {
 
- return (req,res,next)=>{
+  return (req, res, next) => {
 
-    if(!roles.includes(req.user.role)){
-        return res.status(403).json({message:"Access denied"});
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access denied" });
     }
 
     next();
-
- }
-
-}
+  };
+};
