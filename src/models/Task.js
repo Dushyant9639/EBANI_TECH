0e9510d054666aea38c8781fbf72fb203e77f0e9
Dushyant Goyal <goyaldushyant075@gmail.com>
2026-03-30 16:50:38 +0530

    Modified Task Schema

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
-  title:String,
-  description:String,
-  user:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
-  }
-});
+const taskSchema = new mongoose.Schema(
+{
+  title: String,
+  description: String,
 
-module.exports = mongoose.model("Task",taskSchema);
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
